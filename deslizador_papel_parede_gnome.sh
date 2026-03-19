ed2383b04ec81473a7899ece91d9be4cedce6f22
Rodrigo <69372541+RodrigoMtts@users.noreply.github.com>
2026-03-19 04:42:58 -0300

    Update deslizador_papel_parede_gnome.sh

--- deslizador_papel_parede_gnome.sh
+++ deslizador_papel_parede_gnome.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 # 
-# Autor: RodrigoMtts
+# Autor: Rodrigo Soares
+# E-mail: rodrigo.matta.soares@gmail.com
 # 
 # Descrição: Comando para trocar o papel de parede em ambientes gnome. 
-# 		     Todos os papeis de paredes devem estar na pasta /home/$USER/Imagens/slide/
-# 			 Muito últil ao associar a teclas de atalhos.
+# 					 Todos os papeis de paredes devem estar na pasta /home/$USER/Imagens/slide/
+# 					 Muito últil ao associar a teclas de atalhos.
 #
 # Parametros obrigatórios: "+" ou "-"
 
-D_IMAGEM='/home/'$USER'/Imagens/slide/'
+declare -A A_arqs
+declare -a a_arqs
+
+D_IMAGEM="/home/$USER/Imagens/slide/"
 
 if ! [ "$1" = '+' -o "$1" = '-' ] ; then
-    echo "ERRO: Os parametros são obrigatórios. Passe um dos sinais: '+' ou '-'."
+    echo "ERRO: Os parametros são obrigatórios. Passe um dos sinais: +' ou - ."
     exit 1 
 fi
 
 if ! [ -d $D_IMAGEM ] ; then
-    echo 'ERRO: O diretório '$D_IMAGEM' não existe.' 
+    echo "ERRO: O diretório $D_IMAGEM não existe."
     exit 1
 fi
 
-papel_de_parede=$(gsettings get org.gnome.desktop.background picture-uri-dark)
+papel_de_parede="$(gsettings get org.gnome.desktop.background picture-uri-dark)"
+papel_de_parede=${papel_de_parede:1: -1}
 
-quantidade=0
-declare -a arqs
 for arq in $D_IMAGEM* ; do
-    arqs[$quantidade]="$arq"
-    if [ "${papel_de_parede:8}" = "$arq""'" ] ; then
-        posicao=$quantidade
-    fi
-    ((quantidade++))
+    A_arqs[$arq]=${#A_arqs[@]}
+    a_arqs[${#a_arqs[@]}]="$arq"
 done
 
-if [ "${arqs[$posicao]}" = $D_IMAGEM'*' ] ; then
-    echo 'ERRO: O diretório '$D_IMAGEM' está vazio.'
+if [ ${#A_arqs[@]} -eq 0 ] ; then
+    echo "ERRO: O diretório $D_IMAGEM está vazio."
     exit 1
 fi
 
-posicao=$(( ( posicao + quantidade $1 1 ) % quantidade ))
+posicao=$(( ( ${A_arqs[$papel_de_parede]} + ${#A_arqs[@]} $1 1 ) % ${#A_arqs[@]} ))
 
-gsettings set org.gnome.desktop.background picture-uri-dark 'file://'"${arqs[$posicao]}"
+gsettings set org.gnome.desktop.background picture-uri-dark ${a_arqs[$posicao]}
