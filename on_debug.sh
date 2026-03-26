c858bb1e42628c2b6fe0263f0d93477f489b39c9
Rodrigo <69372541+RodrigoMtts@users.noreply.github.com>
2026-03-26 04:58:04 -0300

    Add files via upload

---
+++ on_debug.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# 
+# Autor: Rodrigo Soares
+# 
+# Descrição:  Função externa que mostra os valores das variáveis utilizadas até o momento da chamada.
+#
+# Utilização: Use source para carregar a função e coloque on_debug em qualquer parte do código e, então,
+#             o programa finalizará e mostrará o valor das variáveis instanciadas até o momento
+#
+# Parametros: Todos opcionais;
+#             Ao passar o nome de algumas variável apenas os valores delas serão mostrados; e
+#             Use -r para inverter o dito acima.
+#             
+# ATENÇÃO:    No final dos valores sempre aparecerá um - (traço). Assim múltiplos espaços serão visíveis.
+
+on_debug(){
+    variaveisOnDebug="$(
+        sed -n "1,${BASH_LINENO[0]}p" "${BASH_SOURCE[1]}" |
+        grep -Eo '\b[[:alnum:]_]+=' |
+        sort | 
+        uniq |
+        tr -d '='
+    )"
+
+    if [ "$1" = '-r' ] ; then
+        shift
+        parametrosOnDebug="$(tr ' ' '\n' <<< $*)"
+        variaveisOnDebug="$(sort <<< "$variaveisOnDebug"$'\n'"$parametrosOnDebug" | uniq -u)"
+    elif [ -n "$1" ] ; then
+        parametrosOnDebug="$(tr ' ' '\n' <<< $*)"
+        variaveisOnDebug="$(sort <<< "$variaveisOnDebug"$'\n'"$parametrosOnDebug" | uniq -d)"
+    fi
+    for var in $variaveisOnDebug ; do
+        echo "$var=${!var}"-
+    done
+    exit 0
+}
